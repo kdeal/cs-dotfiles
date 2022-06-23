b77f48bf0b97e9255f395d9c5b3038b6ae6543af
Kyle Deal <kdeal@kyledeal.com>
2022-06-23 21:44:37 +0000

    Add script to install neovim into a codespace
    Made a generic command for install more command line tools. Currently it
    only supports installing neovim.

---
+++ .config/fish/functions/cmd_install.fish
@@ -0,0 +1,18 @@
+function cmd_install --description="Install extra commands that I might want"
+    switch $argv[1]
+        case "nvim"
+            set -l tmp_file (mktemp)
+            curl --progress-bar --output "$tmp_file" --location https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+            set -l file_hash (sha256sum "$tmp_file" | string match --regex "^[0-9a-f]+")
+            echo "Get tar hash from https://github.com/neovim/neovim/releases/nightly"
+            read -l --prompt-str "Enter tar hash: " input_hash
+            if test "$input_hash" = "$file_hash"
+                tar xzf "$tmp_file" -C ~/.cmds/
+                ln -s ~/.cmds/nvim-linux64/bin/nvim ~/.local/bin/
+            else
+                echo "Hashes don't match expected: $input_hash, actual: $file_hash"
+            end
+        case '' '*'
+            echo "Command not recognized: \"$argv[1]\""
+    end
+end
