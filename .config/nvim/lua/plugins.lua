157a4d2c99a8f2619b2c07497f42717fe0ffda7d
Kyle Deal <kdeal@kyledeal.com>
2022-06-24 04:25:51 +0000

    Fix bootstrap script
    The example script wasn't working, but there is an issue about it that
    suggests doing this.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,6 +2,7 @@ local install_path = vim.fn.stdpath('data') ..'/site/pack/packer/start/packer.nv
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
 end
 
 return require('packer').startup(function()
