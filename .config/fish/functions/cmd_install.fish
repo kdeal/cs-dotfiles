e650f9659621c377e59122c90ed0cf5d0f81b856
Kyle Deal <kdeal@kyledeal.com>
2022-07-23 17:49:27 +0000

    Add support for installing ripgrep to cmd_install function
    rg makes find_files work better in telescope and it is what I am used to
    using for filtering things.

--- .config/fish/functions/cmd_install.fish
+++ .config/fish/functions/cmd_install.fish
@@ -1,12 +1,23 @@
 function cmd_install --description="Install extra commands that I might want"
+    function __cmd_install_checksha_download
+        # arg1 = url, arg2 = sha
+        set -l tmp_file (mktemp)
+        curl --progress-bar --output "$tmp_file" --location "$argv[1]"
+        set -l file_hash (sha256sum "$tmp_file" | string match --regex "^[0-9a-f]+")
+        if test "$argv[2]" = "$file_hash"
+            echo $tmp_file
+            return 0
+        else
+            return 1
+        end
+    end
     switch $argv[1]
         case "nvim"
-            set -l tmp_file (mktemp)
-            curl --progress-bar --output "$tmp_file" --location https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-            set -l file_hash (sha256sum "$tmp_file" | string match --regex "^[0-9a-f]+")
-            echo "Get tar hash from https://github.com/neovim/neovim/releases/nightly"
             read -l --prompt-str "Enter tar hash: " input_hash
-            if test "$input_hash" = "$file_hash"
+            set -l url "https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz"
+            set -l tmp_file (__cmd_install_checksha_download $url $input_hash)
+
+            if test -n "$tmp_file"
                 tar xzf "$tmp_file" -C ~/.cmds/
                 ln -s ~/.cmds/nvim-linux64/bin/nvim ~/.local/bin/
                 # Update abbrs to make edit expand to nvim
@@ -14,7 +25,19 @@ function cmd_install --description="Install extra commands that I might want"
                 # Reload editor settings
                 source ~/.config/fish/conf.d/editor.fish
             else
-                echo "Hashes don't match expected: $input_hash, actual: $file_hash"
+                echo "Hashes don't match expected"
+            end
+        case "rg"
+            set -l rg_hash "ee4e0751ab108b6da4f47c52da187d5177dc371f0f512a7caaec5434e711c091"
+            set -l url "https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz"
+            set -l tmp_file (__cmd_install_checksha_download $url $rg_hash)
+            if test -n "$tmp_file"
+                tar xzf "$tmp_file" -C ~/.cmds/
+                ln -s ~/.cmds/ripgrep-13.0.0-x86_64-unknown-linux-musl/rg ~/.local/bin/
+                # Update abbrs to make grep expand to rg
+                update_abbrs
+            else
+                echo "Hashes don't match expected"
             end
         case '' '*'
             echo "Command not recognized: \"$argv[1]\""
