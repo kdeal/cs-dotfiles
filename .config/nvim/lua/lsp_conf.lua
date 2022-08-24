fc6987a8ce5b105cff1c9b5d2dc47164a1b86d66
Kyle Deal <kdeal@kyledeal.com>
2022-08-24 19:14:03 +0000

    Don't set omnifunc when setting up lsp
    I read in the nvim-cmp docs that this is a bad idea.

--- .config/nvim/lua/lsp_conf.lua
+++ .config/nvim/lua/lsp_conf.lua
@@ -4,8 +4,6 @@ local on_attach = function(client, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
     local opts = { noremap=true, silent=true }
     buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
     buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
