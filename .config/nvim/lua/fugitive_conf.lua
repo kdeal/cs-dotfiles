87526b2da5490941b2a4500028edf07ed4618654
Kyle Deal <kdeal@kyledeal.com>
2022-09-27 04:10:21 +0000

    Switch up some fugitive mappings
    Going to try and use fugitive for staging and committing instead of the
    command line.

--- .config/nvim/lua/fugitive_conf.lua
+++ .config/nvim/lua/fugitive_conf.lua
@@ -1,7 +1,5 @@
-noremap('<leader>gs', ':Gstatus<CR>')
-noremap('<leader>gc', ':Gcommit --verbose<CR>')
 noremap('<leader>ge', ':Gedit')
 noremap('<leader>gl', ':Glog')
 noremap('<leader>gb', ':Git blame<CR>')
-noremap('<leader>gd', ':Gdiff<CR>')
+noremap('<leader>gs', ':Git<CR>')
 
