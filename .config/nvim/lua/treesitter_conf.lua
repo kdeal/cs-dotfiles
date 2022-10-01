14185449a7cf8639e250032c498018d613962178
Kyle Deal <kdeal@kyledeal.com>
2022-10-01 15:01:15 +0000

    Disable additional_vim_regex_highlighting
    I only had this turned on so spell checking would only run on comments
    and strings. This now works with treesitter, so I don't need vim regex
    highlighting.

--- .config/nvim/lua/treesitter_conf.lua
+++ .config/nvim/lua/treesitter_conf.lua
@@ -23,7 +23,6 @@ require'nvim-treesitter.configs'.setup {
     },
     highlight = {
         enable = true,
-        additional_vim_regex_highlighting = true,
     },
     indent = {
         enable = true,
