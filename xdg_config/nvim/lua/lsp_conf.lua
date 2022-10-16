1732fbb7107df0c649b0682f7007960236f1bb23
Kyle Deal <kdeal@kyledeal.com>
2022-10-16 04:02:13 +0000

    Use default_capabilities instead update_capabilities for cmp_nvim_lsp
    The update_capabilities function has been deprecated and you should use
    the default_capabilities function now.

--- xdg_config/nvim/lua/lsp_conf.lua
+++ xdg_config/nvim/lua/lsp_conf.lua
@@ -35,7 +35,7 @@ local servers = {
     svelte = {},
     tsserver = {},
 }
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 for lsp, lsp_settings in pairs(servers) do
     settings = { on_attach = on_attach, capabilities = capabilities, settings = lsp_settings }
     nvim_lsp[lsp].setup(settings)
