c1a7bca04d1f90e3cd16a41525ee11dfcf379a1b
Kyle Deal <kdeal@kyledeal.com>
2022-12-24 00:09:02 +0000

    Add pre-commit with filelint

--- xdg_config/fish/functions/fish_mode_prompt.fish
+++ xdg_config/fish/functions/fish_mode_prompt.fish
@@ -1,3 +1,3 @@
 function fish_mode_prompt --description 'Displays the current mode'
-# Do nothing since this is going to be handled by prompt
+    # Do nothing since this is going to be handled by prompt
 end
