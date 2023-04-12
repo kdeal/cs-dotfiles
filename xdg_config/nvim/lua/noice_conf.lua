dba1441a07f72c3f3aac14525ce7a1fbb0978b37
Kyle Deal <kdeal@kyledeal.com>
2023-04-12 05:35:26 +0000

    Modify the symbol for searching down and up
    Use a filled in arrow to make it easier to see and add / to end to make
    the prompt a bit bigger

--- xdg_config/nvim/lua/noice_conf.lua
+++ xdg_config/nvim/lua/noice_conf.lua
@@ -6,8 +6,8 @@ require("noice").setup({
     cmdline = {
         format = {
             cmdline = { icon = ">" },
-            search_down = { icon = "⌄" },
-            search_up = { icon = "⌃" },
+            search_down = { icon = "▼/" },
+            search_up = { icon = "▲/" },
             filter = { icon = "$" },
             lua = { icon = "☾" },
             help = { icon = "?" },
