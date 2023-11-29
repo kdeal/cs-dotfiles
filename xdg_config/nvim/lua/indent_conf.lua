d89c581842ef20a8cbacc825de1665dd51136e1d
Kyle Deal <kdeal@kyledeal.com>
2023-11-29 05:29:57 +0000

    Update all my nvim plugins
    Had to fix indent-blankline and fidget to support their new major
    versions.

--- xdg_config/nvim/lua/indent_conf.lua
+++ xdg_config/nvim/lua/indent_conf.lua
@@ -1,3 +1,3 @@
-require("indent_blankline").setup({
-    show_first_indent_level = false,
-})
+require("ibl").setup()
+local hooks = require("ibl.hooks")
+hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
