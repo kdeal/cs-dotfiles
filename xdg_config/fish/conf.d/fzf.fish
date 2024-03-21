5105d25925a20c5e74d53c5316671df86467b62e
Kyle Deal <kdeal@kyledeal.com>
2024-03-21 03:54:23 +0000

    Switch fzf theme to Dracula

--- xdg_config/fish/conf.d/fzf.fish
+++ xdg_config/fish/conf.d/fzf.fish
@@ -1,25 +1,8 @@
-# colors taken from https://github.com/nicodebo/base16-fzf/blob/master/fish/base16-gruvbox-dark-medium.fish
-set -l color00 '#282828'
-set -l color01 '#3c3836'
-set -l color02 '#504945'
-set -l color03 '#665c54'
-set -l color04 '#bdae93'
-set -l color05 '#d5c4a1'
-set -l color06 '#ebdbb2'
-set -l color07 '#fbf1c7'
-set -l color08 '#fb4934'
-set -l color09 '#fe8019'
-set -l color0A '#fabd2f'
-set -l color0B '#b8bb26'
-set -l color0C '#8ec07c'
-set -l color0D '#83a598'
-set -l color0E '#d3869b'
-set -l color0F '#d65d0e'
-
+# colors taken from https://draculatheme.com/fzf
 set -Ux FZF_COLORS "
-  --color=bg+:$color01,spinner:$color0C,hl:$color0D
-  --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C
-  --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D
+    --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9
+    --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6
+    --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4
 "
 
 set -Ux FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
