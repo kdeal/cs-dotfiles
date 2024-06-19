3d57774d7aa0a4073fc90c5691eb3bf06bb853d9
Kyle Deal <kdeal@kyledeal.com>
2024-06-19 20:41:44 +0000

    Start using fancy icons
    Wezterm makes these work without me doing anything, so might as well use
    them.

--- xdg_config/nvim/lua/plugins/mini.lua
+++ xdg_config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,9 @@
 return {
     {
         "echasnovski/mini.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
         lazy = false,
         keys = {
             -- Have to reenable the plugin, since I disable it during startup
