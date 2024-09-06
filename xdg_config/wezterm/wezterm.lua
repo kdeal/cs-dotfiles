af2279777bc7a4fb86d486bad4b11043cf12559d
Kyle Deal <kdeal@kyledeal.com>
2024-09-06 12:41:11 -0400

    Add some key binds that are more familiar
    I am going to try and use wezterm instead of tmux, so I need some better
    key binds.

--- xdg_config/wezterm/wezterm.lua
+++ xdg_config/wezterm/wezterm.lua
@@ -12,7 +12,26 @@ config.font_size = 13
 config.freetype_load_target = "Normal"
 
 config.keys = {
+    -- Send ssh end session code
     { key = "d", mods = "SHIFT | CMD", action = wezterm.action.SendString("\n~.\n") },
+
+    -- Move to last tab
+    { key = "-", mods = "SUPER", action = wezterm.action.ActivateLastTab },
+
+    -- Move between panes with vim like motions
+    { key = "h", mods = "SUPER", action = wezterm.action.ActivatePaneDirection("Left") },
+    { key = "l", mods = "SUPER", action = wezterm.action.ActivatePaneDirection("Right") },
+    { key = "j", mods = "SUPER", action = wezterm.action.ActivatePaneDirection("Down") },
+    { key = "k", mods = "SUPER", action = wezterm.action.ActivatePaneDirection("Up") },
+
+    -- Move familiar splits
+    { key = "s", mods = "SUPER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "S", mods = "SUPER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "S", mods = "SHIFT | SUPER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+    -- vscode like command palette
+    { key = "p", mods = "SUPER", action = wezterm.action.ActivateCommandPalette },
+
     { key = "-", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
 }
 
