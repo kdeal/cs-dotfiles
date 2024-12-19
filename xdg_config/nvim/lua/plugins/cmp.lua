4a4ec2b7a775a72a6c476d4b8fe8374ead9b1e3c
Kyle Deal <kdeal@kyledeal.com>
2024-12-19 05:37:36 +0000

    Switch to blink.cmp
    It is the new hotness in completions and has better defaults. Also it
    has native support for snippets, so I can get rid of luasnip.

--- xdg_config/nvim/lua/plugins/cmp.lua
+++ xdg_config/nvim/lua/plugins/cmp.lua
@@ -1,39 +1,31 @@
 return {
-    -- Lua completion tool
     {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "saadparwaiz1/cmp_luasnip",
-            "L3MON4D3/LuaSnip",
-        },
-        config = function()
-            local cmp = require("cmp")
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert(),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                    { name = "buffer" },
-                    { name = "path" },
-                }),
-            })
+        "saghen/blink.cmp",
 
-            vim.api.nvim_create_autocmd({ "FileType" }, {
-                desc = "Setup dadbod completion for sql file types",
-                pattern = { "sql", "mysql", "plsql" },
-                callback = function()
-                    require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
-                end,
-            })
-        end,
+        version = "v0.*",
+
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            appearance = {
+                -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+                -- Useful for when your theme doesn't support blink.cmp
+                -- will be removed in a future release
+                use_nvim_cmp_as_default = true,
+            },
+
+            -- default list of enabled providers defined so that you can extend it
+            -- elsewhere in your config, without redefining it, via `opts_extend`
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer", "dadbod", "lazydev" },
+                providers = {
+                    -- dont show LuaLS require statements when lazydev has items
+                    lsp = { fallback_for = { "lazydev" } },
+                    lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+                },
+            },
+
+            signature = { enabled = true },
+        },
     },
 }
