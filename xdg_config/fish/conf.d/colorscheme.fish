0cbb3b0c76ab3eb0bab0a77a06529967812ac2e3
Kyle Deal <kdeal@kyledeal.com>
2025-01-03 20:08:50 +0000

    Make fish shell change the cursor based on mode
    I think I forgot to port this to my cs-dotfiles repo.

--- xdg_config/fish/conf.d/colorscheme.fish
+++ xdg_config/fish/conf.d/colorscheme.fish
@@ -44,3 +44,18 @@ set -U fish_pager_color_progress cyan
 set -U __fish_git_prompt_color_branch brred
 set -U __fish_git_prompt_color_prefix brred
 set -U __fish_git_prompt_color_suffix brred
+
+# Emulates vim's cursor shape behavior
+# Set the normal and visual mode cursors to a block
+set fish_cursor_default block
+# Set the insert mode cursor to a line
+set fish_cursor_insert line
+# Set the replace mode cursors to an underscore
+set fish_cursor_replace_one underscore
+set fish_cursor_replace underscore
+# Set the external cursor to a line. The external cursor appears when a command is started.
+# The cursor shape takes the value of fish_cursor_default when fish_cursor_external is not specified.
+set fish_cursor_external block
+
+# Have to force cursor because it doesn't detect ghostty
+set fish_vi_force_cursor true
