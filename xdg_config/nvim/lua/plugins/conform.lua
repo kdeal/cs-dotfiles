1d16634ae4d73b40301ab1d15c19d56e5fd5c70b
Kyle Deal <kdeal@kyledeal.com>
2025-05-02 03:58:12 +0000

    Turn on format on save
    I have been testing this and like it.

--- xdg_config/nvim/lua/plugins/conform.lua
+++ xdg_config/nvim/lua/plugins/conform.lua
@@ -23,6 +23,10 @@ return {
             },
         },
         opts = {
+            format_on_save = {
+                lsp_format = "fallback",
+                timeout_ms = 500,
+            },
             formatters_by_ft = {
                 fish = { "fish_indent" },
                 graphql = { "prettier" },
