ae7ba13c1c0bf96156c7bb27616004263f2fd977
Kyle Deal <kdeal@kyledeal.com>
2025-05-02 04:58:58 +0000

    Also hide first tab indent level

--- xdg_config/nvim/lua/plugins/init.lua
+++ xdg_config/nvim/lua/plugins/init.lua
@@ -36,6 +36,7 @@ return {
             })
             local hooks = require("ibl.hooks")
             hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+            hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
         end,
     },
 
