0998ff9a5c4114781e8ad3c9bce3c91bd6324e26
Kyle Deal <kdeal@kyledeal.com>
2025-05-03 03:55:51 +0000

    Remove copilot
    I don't plan on using this for personal stuff.

--- xdg_config/nvim/lua/plugins/cmp.lua
+++ xdg_config/nvim/lua/plugins/cmp.lua
@@ -1,26 +1,13 @@
 return {
     {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        build = ":Copilot auth",
-        event = "InsertEnter",
-        opts = {
-            suggestion = { enabled = false },
-        },
-    },
-    {
         "saghen/blink.cmp",
 
         version = "1.*",
-        dependencies = { "fang2hou/blink-copilot" },
 
         ---@module 'blink.cmp'
         ---@type blink.cmp.Config
         opts = {
             appearance = {
-                kind_icons = {
-                    Copilot = "",
-                },
                 -- Sets the fallback highlight groups to nvim-cmp's highlight groups
                 -- Useful for when your theme doesn't support blink.cmp
                 -- will be removed in a future release
@@ -42,17 +29,11 @@ return {
             -- default list of enabled providers defined so that you can extend it
             -- elsewhere in your config, without redefining it, via `opts_extend`
             sources = {
-                default = { "lsp", "path", "snippets", "buffer", "dadbod", "lazydev", "copilot" },
+                default = { "lsp", "path", "snippets", "buffer", "dadbod", "lazydev" },
                 providers = {
                     dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
                     -- dont show LuaLS require statements when lazydev has items
                     lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", fallbacks = { "lsp" } },
-                    copilot = {
-                        name = "copilot",
-                        module = "blink-copilot",
-                        score_offset = 100,
-                        async = true,
-                    },
                 },
             },
 
