9153990392edd55450cbe2a68e5278afc0d31f4f
Kyle Deal <kdeal@kyledeal.com>
2025-07-03 03:19:26 +0000

    Add expand fish from my old dotfiles
    The repo command doesn't work correctly, but it is a start. The main
    thing that I wanted is the gb expand anyways.

---
+++ xdg_config/fish/functions/expand-execute.fish
@@ -0,0 +1,24 @@
+function expand-execute -d 'Executes word expansion on the current token'
+    # Iterate available expansions for one that matches the current token.
+    for expansion in $__expand_expanders
+        set expansion (echo $expansion)
+
+        # Check if the expansion condition matches the token.
+        if eval "$expansion[1]" >/dev/null
+            if [ -n "$expansion[3]" ]
+                set preview "--preview=$expansion[3]"
+            end
+            set replacement (eval "$expansion[2]" | sed '/^\s*$/d' | fzf --ansi --select-1 $preview)
+            set replacement (echo $replacement | eval "$expansion[4]")
+            break
+        end
+    end
+
+    # Interactive filters will cause Fish to need a repaint.
+    commandline -f repaint
+
+    # If a replacement was chosen, use it.
+    if [ -n "$replacement" ]
+        commandline -t -r "$replacement"
+    end
+end
--- xdg_config/fish/functions/fish_user_key_bindings.fish
+++ xdg_config/fish/functions/fish_user_key_bindings.fish
@@ -3,6 +3,11 @@ function fish_user_key_bindings
         bind --mode $mode \cg nextd-or-forward-word
         bind --mode $mode \cf forward-char
     end
+
+    for mode in normal insert visual
+        bind --mode $mode \cn expand-execute
+        bind --mode $mode \cp expand-execute
+    end
 end
 
 fzf --fish | source
