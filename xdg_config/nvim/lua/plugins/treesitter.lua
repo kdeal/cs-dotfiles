afbc140dc725449be3115975d6d3e72819794957
Kyle Deal <kdeal@kyledeal.com>
2026-02-21 17:07:49 +0000

    Switch treesitter to master
    I need to do somethings to get it to work on the default main branch.

--- xdg_config/nvim/lua/plugins/treesitter.lua
+++ xdg_config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
     -- Tree sitter based syntax highlighting
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = 'master',
         build = function()
             require("nvim-treesitter.install").update({ with_sync = true })
         end,
